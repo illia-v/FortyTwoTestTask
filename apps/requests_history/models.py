b9abb98765f3c1c2664bd25e071b2fd540680644
Illia Volochii <illia.volochii@gmail.com>
2016-10-13 18:59:23 +0300

    ticket:3 Clean the model `Request`

--- apps/requests_history/models.py
+++ apps/requests_history/models.py
@@ -5,7 +5,6 @@ class Request(models.Model):
     url = models.URLField()
     timestamp = models.DateTimeField(auto_now_add=True)
     method = models.CharField(max_length=20)
-    priority = models.PositiveSmallIntegerField(default=1)
 
     def __unicode__(self):
         return u'%s %s %s' % (self.timestamp.isoformat(),
