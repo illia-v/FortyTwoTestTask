729869aeb01ebf92def10c3597565d5b589604d7
Illia Volochii <illia.volochii@gmail.com>
2016-10-13 14:00:37 +0300

    ticket:1 Test that `HelloView` returns `PersonInfo` instance

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 
+from .model_instances import person_info
+from ..models import PersonInfo
+
 
 class TestHelloView(TestCase):
     """
     A test case for a view `HelloView`
     """
     def setUp(self):
+        person_info()
         self.response = self.client.get('/')
 
     def test_hello_view_basic(self):
@@ -18,14 +22,24 @@ class TestHelloView(TestCase):
         self.assertEqual(self.response.status_code, 200,
                          'Should be callable by anyone')
 
+    def test_hello_view_returns_my_info(self):
+        """
+        Ensures that `HelloView` returns `PersonInfo` instance `my_info`
+        in context
+        """
+        my_info = self.response.context_data['my_info']
+        self.assertIs(type(my_info), PersonInfo, 'Should return a '
+                      '`PersonInfo` instance `my_info` in context')
+
     def test_hello_view_template_output(self):
         """
         Ensures that output of a `HelloView` teplate is valid
         """
         resopnse_content = self.response.content
-        self.assertIn('Illia', resopnse_content,
+        person = PersonInfo.objects.first()
+        self.assertIn(person.first_name, resopnse_content,
                       "Person's first name should be in the template")
-        self.assertIn('Volochii', resopnse_content,
+        self.assertIn(person.second_name, resopnse_content,
                       "Person's last name should be in the template")
-        self.assertIn('illia.volochii@gmail.com', resopnse_content,
+        self.assertIn(person.email, resopnse_content,
                       "Person's email should be in the template")
