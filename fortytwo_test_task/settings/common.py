8b2bf5de8e5365968544942bac7ae5c7fa5e22d2
Illia Volochii <illia.volochii@gmail.com>
2016-10-13 18:56:47 +0300

    ticket:3 Code `RequestSavingMiddleware`. Add it to `MIDDLEWARE_CLASSES`

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -53,6 +53,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.requests_history.middleware.RequestSavingMiddleware',
 )
 
 ROOT_URLCONF = 'fortytwo_test_task.urls'
