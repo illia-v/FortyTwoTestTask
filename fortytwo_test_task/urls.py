b5949601e9d906454d1a0d23b75e872ec10dd247
Illia Volochii <illia.volochii@gmail.com>
2016-10-22 00:24:09 +0300

    ticket:5 Add `MEDIA_URL` to the URL patterns

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -27,4 +29,4 @@ urlpatterns = patterns(
     url(r'^pull_new_requests/$',
         requests_hisory_views.RequestsPullingView.as_view(),
         name='pull_new_requests')
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
