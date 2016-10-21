fdf548e50eb6a0563b3f9203c39a3c7d47629073
Illia Volochii <illia.volochii@gmail.com>
2016-10-21 20:06:48 +0300

    ticket:5 Test that `PersonInfo` photo is scaled to 200x200

--- apps/hello/tests/test_models.py
+++ apps/hello/tests/test_models.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from StringIO import StringIO
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.test import TestCase
 
 from . import model_instances
+from .image import TEST_IMAGE
 
 
 class TestPersonInfo(TestCase):
@@ -28,3 +32,24 @@ class TestPersonInfo(TestCase):
         self.assertEqual(self.person_info.__unicode__(), u'Їя Їя',
                          'Method `__unicode__` of the `PersonInfo` model '
                          'should return a valid string')
+
+    def test_person_info_scales_photo_to_200x200(self):
+        """
+        Ensures `PersonInfo` photo is scaled to 200x200
+        """
+        self.person_info = model_instances.person_info()
+        self.person_info.photo = InMemoryUploadedFile(
+            StringIO(TEST_IMAGE),
+            field_name='photo',
+            name='mt_photo.jpg',
+            content_type='image/jpg',
+            size=len(TEST_IMAGE),
+            charset='utf-8',
+        )
+        self.person_info.save()
+
+        photo = self.person_info.photo
+
+        self.assertLessEqual(photo.width and photo.height, 200,
+                             'Photo should be scaled to 200x200, maintaining '
+                             'aspect ratio, before saving')
