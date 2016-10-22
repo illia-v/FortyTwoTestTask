93d944456ef6ba8e397d9bd7815e6382af28cd07
Illia Volochii <illia.volochii@gmail.com>
2016-10-22 17:36:28 +0300

    ticket:8 Create the signal handler

---
+++ apps/general/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from .models import ActionOnInstance
+
+
+@receiver(post_save)
+@receiver(post_delete)
+def create_db_entity_about_action_on_instance(sender, **kwargs):
+    if sender.__name__ == 'ActionOnInstance':
+        return
+
+    if kwargs.get('created', None) is not None:
+        action = 1 if kwargs['created'] else 2
+    else:
+        action = 3
+
+    ActionOnInstance.objects.create(
+        app_name=sender._meta.app_label,
+        model_name=sender.__name__,
+        model_id=kwargs['instance'].id,
+        instance=str(kwargs['instance'].__dict__),
+        action=action
+    )
