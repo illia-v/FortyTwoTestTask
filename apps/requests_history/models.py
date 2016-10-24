47cf26bb87597cab68ad18a2f328568da1e9ed9b
Illia Volochii <illia.volochii@gmail.com>
2016-10-24 19:43:25 +0300

    ticket:13 Add a field `priority` to the model `Request`

--- apps/requests_history/models.py
+++ apps/requests_history/models.py
@@ -5,6 +5,7 @@ class Request(models.Model):
     url = models.URLField()
     timestamp = models.DateTimeField(auto_now_add=True)
     method = models.CharField(max_length=20)
+    priority = models.PositiveSmallIntegerField(default=1)
 
     def __unicode__(self):
         return u'%s %s %s' % (self.timestamp.isoformat(),
