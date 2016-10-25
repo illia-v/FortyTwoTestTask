92de255152f7e9812bdc491f59fef62dea5360e2
Illia Volochii <illia.volochii@gmail.com>
2016-10-25 20:37:42 +0300

    ticket:5 Create the validator `validate_birth_date`

---
+++ apps/hello/validators.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
+
+
+def validate_birth_date(birth_date):
+    if now().date() < birth_date:
+        raise ValidationError('You can not be born in the future')
+
+    today_minus_120_years = now().date().replace(year=now().year-120)
+
+    if birth_date < today_minus_120_years:
+        raise ValidationError('You can not be more than 120 years old')
