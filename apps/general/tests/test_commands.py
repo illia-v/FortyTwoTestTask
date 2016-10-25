17f34c5ee1345f80dce2821abdb21ac9c5d91f72
Illia Volochii <illia.volochii@gmail.com>
2016-10-25 15:29:05 +0300

    ticket:8 Improve `TestPrintAllModels`

--- apps/general/tests/test_commands.py
+++ apps/general/tests/test_commands.py
@@ -5,12 +5,11 @@ from django.db import models
 from django.core.management import call_command
 from django.test import TestCase
 
-from hello.models import PersonInfo
-
 
 class TestPrintAllModels(TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.all_models = models.get_models(include_auto_created=True)
         # Transfer of stdout and stderr to variables
         cls.stdout = sys.stdout = StringIO()
         cls.stderr = sys.stderr = StringIO()
@@ -26,30 +25,25 @@ class TestPrintAllModels(TestCase):
         Ensures that `print_all_models` prints both names of models and
         the count of objects in every model to stdout
         """
-        self.assertIn(
-            'Model `PersonInfo` - %d instances' % PersonInfo.objects.count(),
-            self.stdout.getvalue(), 'Should write names of models and the '
-            'count of objects in every model to stdout'
-        )
+        stdout_value = self.stdout.getvalue()
+        for model in self.all_models:
+            self.assertIn(
+                'Model `%s` - %d instances' % (model.__name__,
+                                               model.objects.count()),
+                stdout_value, 'Should write names of models and the count of '
+                'objects in every model to stdout'
+            )
 
     def test_print_all_models_writes_to_stderr(self):
         """
         Ensures that `print_all_models` prints both names of models and
         the count of objects in every model to stderr
         """
-        self.assertIn(
-            'Model `PersonInfo` - %d instances' % PersonInfo.objects.count(),
-            self.stdout.getvalue(), 'Should write names of models and the '
-            'count of objects in every model to stderr'
-        )
-
-    def test_print_all_models_writes_about_all_models(self):
-        """
-        Ensures that `print_all_models` prints information about all
-        models of the project
-        """
-        self.assertEqual(
-            self.stderr.getvalue().count('error:'),
-            len(models.get_models(include_auto_created=True)),
-            "Should write about all models"
-        )
+        stderr_value = self.stderr.getvalue()
+        for model in self.all_models:
+            self.assertIn(
+                'error: Model `%s` - %d instances' % (model.__name__,
+                                                      model.objects.count()),
+                stderr_value, 'Should write names of models and the count of '
+                'objects in every model to stderr'
+            )
