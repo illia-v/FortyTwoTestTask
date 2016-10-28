15324759bc66d9c0d0e24f21405d22d00f6bb984
Illia Volochii <illia.volochii@gmail.com>
2016-10-28 21:04:54 +0300

    ticket:8 Make the template tag `edit_link` accept only model instances

--- apps/general/templatetags/edit_link.py
+++ apps/general/templatetags/edit_link.py
@@ -1,4 +1,3 @@
-
 from django import template
 from django.core.urlresolvers import reverse
 
@@ -11,6 +10,11 @@ def edit_link(model_instance):
     A template tag which accepts any object and renders a link to its
     admin edit page
     """
-    return reverse('admin:%s_%s_change' % (model_instance._meta.app_label,
-                                           model_instance._meta.module_name),
-                   args=(model_instance.pk,))
+    try:
+        return reverse(
+            'admin:%s_%s_change' % (model_instance._meta.app_label,
+                                    model_instance._meta.module_name),
+            args=(model_instance.pk,)
+        )
+    except AttributeError:
+        raise ValueError('The tag accepts only a model instance')
