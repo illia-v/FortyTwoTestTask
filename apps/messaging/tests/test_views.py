2a92f998d5e93ff4c4d31e1681d8df069b1a820e
Illia Volochii <illia.volochii@gmail.com>
2016-11-09 20:38:53 +0200

    ticket:17 Test that the views return real data

--- apps/messaging/tests/test_views.py
+++ apps/messaging/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
 from django.test import RequestFactory, TestCase
 
+from ..forms import MessageForm
 from ..views import MessagingDetailView, MessagingIndexView
 
 
@@ -10,19 +12,18 @@ class TestMessagingIndexView(TestCase):
     """
     def setUp(self):
         self.user = User.objects.create(username='test', password='testpswd')
+        request = RequestFactory().get('/messaging/')
+        request.user = self.user
+        self.response = MessagingIndexView.as_view()(request)
 
     def test_messaging_index_view_basic(self):
         """
         Ensures that `MessagingIndexView` uses an appropriate template
         and authenticated users can get its response
         """
-        request = RequestFactory().get('/messaging/')
-        request.user = self.user
-        response = MessagingIndexView.as_view()(request)
-
-        self.assertTemplateUsed(response, 'messages/index.html',
+        self.assertTemplateUsed(self.response, 'messages/index.html',
                                 'Should use an appropriate template')
-        self.assertEqual(response.status_code, 200,
+        self.assertEqual(self.response.status_code, 200,
                          'Should be callable by a registered user')
 
     def test_anonymous(self):
@@ -34,30 +35,42 @@ class TestMessagingIndexView(TestCase):
         self.assertIn('login', response_for_anonymous.url,
                       'Should redirect to login')
 
+    def test_messaging_index_view_returns_interlocutors(self):
+        """
+        Ensures that `MessagesIndexView` returns `QuerySet`
+        `interlocutors_with_unread_messages_count` in context
+        """
+        interlocutors_with_unread_messages_count = self.response.context_data[
+            'interlocutors_with_unread_messages_count'
+        ]
+
+        self.assertIs(type(interlocutors_with_unread_messages_count), list,
+                      'Should return `list` '
+                      '`interlocutors_with_unread_messages_count` in context')
+
 
 class TestMessagingDetailView(TestCase):
     """
     A test case for a view `MessagingDetailView`
     """
     def setUp(self):
-        self.user = User.objects.create(username='test', password='testpswd')
         self.interlocutor = User.objects.create(username='test1',
                                                 password='testpswd')
+        request = RequestFactory().get(
+            '/messaging/%s/' % self.interlocutor.username
+        )
+        request.user = User.objects.create(username='test',
+                                           password='testpswd')
+        self.response = MessagingDetailView.as_view()(request)
 
     def test_messaging_detail_view_basic(self):
         """
         Ensures that `MessagingDetailView` uses an appropriate template and
         authenticated users can get its response
         """
-        request = RequestFactory().get(
-            '/messaging/%s/' % self.interlocutor.username
-        )
-        request.user = self.user
-        response = MessagingDetailView.as_view()(request)
-
-        self.assertTemplateUsed(response, 'messaging/detail.html',
+        self.assertTemplateUsed(self.response, 'messaging/detail.html',
                                 'Should use an appropriate template')
-        self.assertEqual(response.status_code, 200,
+        self.assertEqual(self.response.status_code, 200,
                          'Should be callable by a registered user')
 
     def test_anonymous(self):
@@ -70,3 +83,21 @@ class TestMessagingDetailView(TestCase):
         )
         self.assertIn('login', response_for_anonymous.url,
                       'Should redirect to login')
+
+    def test_messaging_index_view_returns_messages(self):
+        """
+        Ensures that `MessagesIndexView` returns `QuerySet` `messages`
+        in context
+        """
+        messages = self.response.context_data['messages']
+        self.assertIs(type(messages), QuerySet,
+                      'Should return `QuerySet` `messages` in context')
+
+    def test_messaging_index_view_returns_message_form(self):
+        """
+        Ensures that `MessagesIndexView` returns a `MessageForm`
+        instance in context
+        """
+        message_form = self.response.context_data['form']
+        self.assertIs(type(message_form), MessageForm,
+                      'Should return a `MessageForm` instance in context')
