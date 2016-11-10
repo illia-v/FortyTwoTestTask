57720369eb775c9d21c5b11873d93f40c6f60bf0
Illia Volochii <illia.volochii@gmail.com>
2016-11-10 23:25:07 +0200

    ticket:17 Create `MessagingCreateView` & `MessagingPullView` and their URL patterns

--- apps/messaging/views.py
+++ apps/messaging/views.py
@@ -1,10 +1,14 @@
+import json
+
+from django import http
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
-from django.views.generic import FormView, TemplateView
+from django.utils.formats import date_format
+from django.views.generic import FormView, TemplateView, View
 
 from .forms import MessageForm
-from .models import Conversation
+from .models import Conversation, Message
 
 
 class MessagingIndexView(TemplateView):
@@ -70,3 +74,93 @@ class MessagingDetailView(FormView):
         context['all_messages'] = self.get_messages()
         context['interlocutor_username'] = self.get_interlocutor_username()
         return context
+
+
+class MessagingCreateView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            return http.HttpResponseBadRequest()
+
+        try:
+            message_body = request.POST['message']
+        except KeyError:
+            return http.HttpResponseBadRequest()
+
+        form = MessageForm({'message': message_body})
+        if not form.is_valid():
+            return http.HttpResponse(json.dumps(form.errors),
+                                     content_type='application/json')
+
+        # interlocutor's username from a path of a request
+        interlocutors_username = request.path.split('/')[2]
+        interlocutor = get_object_or_404(User, username=interlocutors_username)
+
+        try:
+            conversation = Conversation.objects.filter(
+                interlocutors__exact=request.user
+            ).select_related('message_set').get(
+                interlocutors__exact=interlocutor
+            )
+        except ObjectDoesNotExist:
+            raise http.Http404
+
+        message = Message.objects.create(
+            conversation=conversation,
+            sender=request.user,
+            body=message_body
+        )
+
+        message_dict = {
+            'id': message.id,
+            'sender': message.sender.username,
+            'body': message.body,
+            'timestamp': date_format(message.timestamp, 'DATETIME_FORMAT')
+        }
+
+        return http.HttpResponse(json.dumps(message_dict),
+                                 content_type='application/json')
+
+
+class MessagingPullView(View):
+    def get(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            return http.HttpResponseBadRequest()
+
+        try:
+            last_message_id = int(request.GET['last_message_id'])
+        except (KeyError, ValueError):
+            return http.HttpResponseBadRequest()
+
+        # interlocutor's username from a path of a request
+        interlocutors_username = request.path.split('/')[2]
+        interlocutor = get_object_or_404(User, username=interlocutors_username)
+
+        try:
+            messages = Conversation.objects.filter(
+                interlocutors__exact=request.user
+            ).select_related('message_set').get(
+                interlocutors__exact=interlocutor
+            ).message_set.all()
+        except ObjectDoesNotExist:
+            raise http.Http404
+
+        # If a clients page is up to date
+        if messages.last() is None or messages.last().id == last_message_id:
+            return http.HttpResponse('[]', content_type='application/json')
+
+        new_messages = []
+
+        for new_message in messages.filter(id__gt=last_message_id).\
+                order_by('id'):
+            sender = (new_message.sender.username
+                      if new_message.sender != request.user else 'You')
+            new_messages.append({
+                'id': new_message.id,
+                'sender': sender,
+                'body': new_message.body,
+                'timestamp': date_format(new_message.timestamp,
+                                         'DATETIME_FORMAT')
+            })
+
+        return http.HttpResponse(json.dumps(new_messages),
+                                 content_type='application/json')
