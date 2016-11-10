1b2ae09e7a443274d8e583271619a1f6673cffd9
Illia Volochii <illia.volochii@gmail.com>
2016-11-10 22:45:40 +0200

    ticket:17 Create tests for URL patterns of  new views

--- apps/messaging/tests/test_urls.py
+++ apps/messaging/tests/test_urls.py
@@ -29,3 +29,31 @@ class TestMessagingURLs(TestCase):
                          'to the URL `/messaging/{username}/`')
         self.assertEqual(messages_detail.func.__name__, 'MessagingDetailView',
                          'Should be resolved to `MessagingDetailView`')
+
+    def test_messaging_create_view_url(self):
+        """
+        Ensures that a URL pattern name `messaging:create` is valid and
+        the pattern is resolved to `MessagingCreateView`
+        """
+        messaging_create = resolve('/messaging/somebody/new/')
+
+        self.assertEqual(reverse('messaging:create', args=['somebody']),
+                         '/messaging/somebody/new/',
+                         'A view name `messaging:create` should be reversed '
+                         'to the URL `/messaging/{username}/new/`')
+        self.assertEqual(messaging_create.func.__name__, 'MessagingCreateView',
+                         'Should be resolved to `MessagingCreateView`')
+
+    def test_messaging_pull_view_url(self):
+        """
+        Ensures that a URL pattern name `messaging:pull` is valid and
+        the pattern is resolved to `MessagingPullView`
+        """
+        messaging_pull = resolve('/messaging/somebody/pull/')
+
+        self.assertEqual(reverse('messaging:pull', args=['somebody']),
+                         '/messaging/somebody/pull/',
+                         'A view name `messaging:pull` should be reversed '
+                         'to the URL `/messaging/{username}/pull/`')
+        self.assertEqual(messaging_pull.func.__name__, 'MessagingPullView',
+                         'Should be resolved to `MessagingPullView`')
